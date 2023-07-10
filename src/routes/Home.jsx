1d6c03c9065f960c6c3856265a8728a33a7fb04e
Anita Sharma <sharma.anita00001@gmail.com>
2023-07-10 07:35:41 +0545

    React Router

---
+++ src/routes/Home.jsx
@@ -0,0 +1,16 @@
+import Header from '../components/Header';
+import TodosLogic from '../components/TodosLogic';
+import '../styles/TodoApp.css';
+
+function Home() {
+  return (
+    <div className="wrapper">
+      <div className="todos">
+        <Header />
+        <TodosLogic />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
